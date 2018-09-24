b9ba64cb6696b9a04b4682f8b4969143fb19c23d
Micah Koki <micahkoki@Micahs-MacBook-Pro.local>
2018-09-23 21:16:05 -0700

    Back to the Lab

---
+++ test.js
@@ -0,0 +1,21 @@
+// Test Atom Runner.
+// Basic arrow function.
+
+console.log("I have the POWER!");
+
+var adventures = [];
+var recentAdventure = "Hello World";
+
+var addStory = story => {
+  adventures.push(story);
+  return story;
+}
+
+var tellRecentStory = adventures => {
+  return adventures[adventures.length - 1];
+}
+
+addStory(recentAdventure);
+tellRecentStory(adventures);
+
+console.log(tellRecentStory(adventures));
